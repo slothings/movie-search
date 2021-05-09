ebe7ab8e4aec1a1ee665b09111798e4bbe4a920c
Slothing <seth.r.martineau@gmail.com>
2021-05-08 22:47:38 -0600

    deleted useless files

--- src/components/Nominate/Nominate.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Nominate = () => {
-  return (
-      <button className="mr-2">Nominate</button>
-  )
-}
-
-export default Nominate;
--- src/components/RemoveMovie/RemoveMovie.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const RemoveMovie = () => {
-  return (
-    <button className="mr-2">Remove</button>
-  )
-}
-
-export default RemoveMovie;
